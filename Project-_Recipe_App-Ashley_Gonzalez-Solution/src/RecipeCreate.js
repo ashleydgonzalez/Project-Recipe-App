ca04628e7394bca1e539a51d88064c757308b49c
Ashley Gonzalez <114032815+ashleydgonzalez@users.noreply.github.com>
2022-11-27 22:35:05 -0800

    Add files via upload

---
+++ Project-_Recipe_App-Ashley_Gonzalez-Solution/src/RecipeCreate.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+
+function RecipeCreate({recipes, createRecipe}) {
+
+  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+  // TODO: Add the required input and textarea form elements.
+  // TODO: Add the required submit and change handlers
+  const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+  
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const onChange = ({target}) => setFormData({ ...formData, [target.name]: target.value });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createRecipe(formData);
+    
+    setFormData({ ...initialFormState });
+  };
+  
+  return (
+    <form name="create" onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <input
+                onChange={onChange}
+                value={formData.name}
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Name"
+                required={true}
+                />
+            </td>
+            <td>
+              <input
+                onChange={onChange}
+                value={formData.cuisine}
+                id="cuisine"
+                name="cuisine"
+                type="text"
+                placeholder="Cuisine"
+                required={true}
+                />
+              </td>
+             <td>
+              <input
+                onChange={onChange}
+                value={formData.photo}
+                id="photo"
+                name="photo"
+                type="url"
+                placeholder="URL"
+                required={true}
+              />
+            </td>
+             <td>
+              <textarea
+                onChange={onChange}
+                value={formData.ingredients}
+                id="ingredients"
+                name="ingredients"
+                placeholder="Ingredients"
+                required={true}
+                rows={3}
+              />
+            </td>
+            <td>
+              <textarea
+                onChange={onChange}
+                value={formData.preparation}
+                id="preparation"
+                name="preparation"
+                placeholder="Preparation"
+                required={true}
+                rows={3}
+              />
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
---
+++ Project-_Recipe_App-Ashley_Gonzalez-Solution/src/RecipeList.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function RecipeList({ recipes, createRecipe, deleteRecipe }) {
+  function handleDeleteOnClick(index) {
+    deleteRecipe(index);
+  };
+  
+  let rows = recipes.map(({ name, cuisine, photo, ingredients, preparation }, index ) => {
+    return (
+    <tr key={index}>
+        <td>{name}</td>
+        <td>{cuisine}</td>
+        <td>
+          <img src={photo} alt="Recipe" />
+        </td>
+        <td className="content_td">
+          <p>{ingredients}</p>
+        </td>
+        <td className="content_td">
+          <p>{preparation}</p>
+        </td>
+        <td>
+          <button name="delete" onClick={() => handleDeleteOnClick(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+  
+  // TODO: Display the list of recipes using the structure of table that is provided.
+  // TODO: Create at least one additional component that is used by this component.
+  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+
+  return (
+    <div className="recipe-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RecipeList;
